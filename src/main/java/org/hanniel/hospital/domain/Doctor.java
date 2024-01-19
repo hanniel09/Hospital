0deba7bb0a7ea7726e60c20f7fe55e17cafba878
hanniel09 <hannielvieria1227@gmail.com>
2024-01-19 17:29:01 -0300

    feat: create model and dto

---
+++ src/main/java/org/hanniel/hospital/domain/Doctor.java
@@ -0,0 +1,81 @@
+package org.hanniel.hospital.domain;
+
+import jakarta.persistence.*;
+import org.hanniel.hospital.dtos.DoctorDTO;
+
+@Table
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long DVC;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String specialty;
+
+    @Column(nullable = false)
+    private String room;
+
+    public Doctor() {
+    }
+
+    public Doctor(DoctorDTO data) {
+        this.DVC = data.DVC();
+        this.name = data.name();
+        this.specialty = data.specialty();
+        this.room = data.room();
+    }
+
+    public Doctor(Long DVC, String name, String specialty, String room) {
+        this.DVC = DVC;
+        this.name = name;
+        this.specialty = specialty;
+        this.room = room;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getDVC() {
+        return DVC;
+    }
+
+    public void setDVC(Long DVC) {
+        this.DVC = DVC;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+}
---
+++ src/main/java/org/hanniel/hospital/dtos/DoctorDTO.java
@@ -0,0 +1,4 @@
+package org.hanniel.hospital.dtos;
+
+public record DoctorDTO(Long DVC, String name, String specialty, String room) {
+}
