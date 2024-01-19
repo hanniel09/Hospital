fbf7f1882008ad3f367ffce0a862aefcbf82509e
hanniel09 <hannielvieria1227@gmail.com>
2024-01-19 17:29:25 -0300

    feat: create doctor service

---
+++ src/main/java/org/hanniel/hospital/services/DoctorService.java
@@ -0,0 +1,43 @@
+package org.hanniel.hospital.services;
+
+import org.hanniel.hospital.domain.Doctor;
+import org.hanniel.hospital.dtos.DoctorDTO;
+import org.hanniel.hospital.exceptions.BadRequestException;
+import org.hanniel.hospital.repositories.DoctorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DoctorService {
+
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    public List<Doctor> findAllDoctors(){
+        return doctorRepository.findAll();
+    }
+
+    public Doctor findDoctorById(Long id) {
+        return doctorRepository.findById(id).
+                orElseThrow(() -> new BadRequestException("Doctor", id));
+    }
+
+    public Doctor createDoctor(DoctorDTO data){
+        Doctor doctor = new Doctor(data);
+        return doctorRepository.save(doctor);
+    }
+
+    public Doctor updateDoctor(DoctorDTO data, Long id){
+        Doctor oldDoctor = findDoctorById(id);
+        Doctor newDoctor = new Doctor(data);
+        newDoctor.setId(oldDoctor.getId());
+        return doctorRepository.save(newDoctor);
+    }
+
+    public void deleteDoctor(Long id){
+        doctorRepository.deleteById(id);
+    }
+
+}
