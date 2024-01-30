7a98e91ede8b57bda4b8cae6123e32f697be6dab
Hanniel Vieira <hannielvieira1227@gmail.com>
2024-01-30 17:09:46 -0300

    refact: UrgencyQueueDTO

--- src/main/java/org/hanniel/hospital/dtos/UrgencyQueueDTO.java
+++ src/main/java/org/hanniel/hospital/dtos/UrgencyQueueDTO.java
@@ -2,7 +2,9 @@ package org.hanniel.hospital.dtos;
 
 import org.hanniel.hospital.domain.Patient;
 
+import java.util.List;
 import java.util.Set;
 
-public record UrgencyQueueDTO(String name, Set<Patient> patients) {
+public record UrgencyQueueDTO(String name, List<Long> patients) {
+
 }
