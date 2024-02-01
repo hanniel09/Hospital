ed1af54cdef8a22bd03e97c12f7370c084d0fe97
Hanniel Vieira <hannielvieira1227@gmail.com>
2024-02-01 18:01:06 -0300

    feat: create new methods to DoctorRepository

--- src/main/java/org/hanniel/hospital/repositories/DoctorRepository.java
+++ src/main/java/org/hanniel/hospital/repositories/DoctorRepository.java
@@ -3,5 +3,10 @@ package org.hanniel.hospital.repositories;
 import org.hanniel.hospital.domain.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+    Optional<Doctor> findDoctorByName(String name);
+
+    Optional<Doctor> findDoctorByDVC(Long DVC);
 }
