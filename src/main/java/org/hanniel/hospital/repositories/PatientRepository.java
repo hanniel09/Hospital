3f27dfcd257609f3fa6dc2b53b35bdf3d40a1ec7
Hanniel Vieira <hannielvieira1227@gmail.com>
2024-02-01 18:01:19 -0300

    feat: create new methods to PatientRepository

--- src/main/java/org/hanniel/hospital/repositories/PatientRepository.java
+++ src/main/java/org/hanniel/hospital/repositories/PatientRepository.java
@@ -3,5 +3,11 @@ package org.hanniel.hospital.repositories;
 import org.hanniel.hospital.domain.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Optional<Patient> findPatientByName(String name);
+
+    Optional<Patient> findPatientByAttendanceSheet(Long AttendanceSheet);
 }
